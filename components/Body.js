8e5860d9d8e05ec213127a8e1bc9ac810e37d06a
proforty7 <proforty7@gmail.com>
2019-10-30 01:02:45 +0530

    add 3 sections in body
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- components/Body.js
+++ components/Body.js
@@ -1,52 +1,22 @@
-import React, { Component, useEffect, useState } from "react";
-import { Text, View, ActivityIndicator, StyleSheet } from "react-native";
-import api from "../api";
-import { API_KEY } from "../config/keys";
+import React from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Section from "./Section";
 
 const Body = () => {
-  const [loading, setLoading] = useState(true);
-  const [popularList, setPopularList] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await api.get("/movie/popular", {
-          params: { api_key: API_KEY }
-        });
-        const data = res.data.results.map(item => {
-          return {
-            title: item.title,
-            vote_average: item.vote_average,
-            backdrop_path: item.backdrop_path
-          };
-        });
-        setPopularList(data);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
+  // const [loading, setLoading] = useState(true);
 
   return (
-    <View style={styles.container}>
-      {loading ? (
-        <ActivityIndicator size="large" color="#c0392b" />
-      ) : (
-        <Section title="Most Popular" list={popularList} />
-      )}
-    </View>
+    <ScrollView style={styles.container}>
+      <Section category="now_playing" title="Now Playing" />
+      <Section category="popular" title="Popular" />
+      <Section category="top_rated" title="Top Rated" />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    flex: 1
   }
 });
 
--- components/Section.js
+++ components/Section.js
@@ -1,39 +1,81 @@
-import React, { Component } from "react";
-import { Text, StyleSheet, View, FlatList } from "react-native";
+import React, { Component, useEffect, useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  ActivityIndicator
+} from "react-native";
 import { Card } from "react-native-elements";
+import api from "../api";
+import { API_KEY } from "../config/keys";
 
-export default Section = ({ list, title }) => {
-  // console.log(list);
+export default Section = ({ category, title }) => {
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await api.get(`/movie/${category}`, {
+          params: { api_key: API_KEY }
+        });
+        const data = res.data.results.map(item => {
+          return {
+            title: item.title,
+            vote_average: item.vote_average,
+            backdrop_path: item.backdrop_path
+          };
+        });
+        setList(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [category]);
   return (
-    <View>
-      <Text style={styles.heading}>{title}</Text>
-      <FlatList
-        horizontal
-        data={list}
-        renderItem={movie => {
-          return (
-            <Card
-              image={{
-                uri: `https://image.tmdb.org/t/p/w300/${movie.item.backdrop_path}`
-              }}
-              featuredTitle={movie.item.title}
-              featuredSubtitle={movie.item.vote_average.toString()}
-              imageStyle={styles.image}
-              imageProps={{ resizeMode: "cover" }}
-              featuredTitleStyle={styles.title}
-              featuredSubtitleStyle={styles.subtitle}
-              containerStyle={styles.card}
-              imageWrapperStyle={{ borderRadius: 5 }}
-            />
-          );
-        }}
-        keyExtractor={(item, i) => `${i}`}
-      />
+    <View style={styles.container}>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <View>
+          <Text style={styles.heading}>{title}</Text>
+          <FlatList
+            horizontal
+            data={list}
+            renderItem={movie => {
+              return (
+                <Card
+                  image={{
+                    uri: `https://image.tmdb.org/t/p/w300/${movie.item.backdrop_path}`
+                  }}
+                  featuredTitle={movie.item.title}
+                  featuredSubtitle={movie.item.vote_average.toString()}
+                  imageStyle={styles.image}
+                  imageProps={{ resizeMode: "cover" }}
+                  featuredTitleStyle={styles.title}
+                  featuredSubtitleStyle={styles.subtitle}
+                  containerStyle={styles.card}
+                  imageWrapperStyle={{ borderRadius: 5 }}
+                />
+              );
+            }}
+            keyExtractor={(item, i) => `${i}`}
+          />
+        </View>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginBottom: 15
+  },
   heading: {
     color: "#fff",
     fontWeight: "bold",
