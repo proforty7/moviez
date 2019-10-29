88cddd105cf85ebf9c612f7212b87a94d56e43cd
proforty7 <proforty7@gmail.com>
2019-10-30 01:46:13 +0530

    add search bar
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 import { ThemeProvider } from "react-native-elements";
 import MHeader from "./components/MHeader";
 import Body from "./components/Body";
@@ -13,6 +13,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
       <ThemeProvider theme={theme}>
         <MHeader />
         <Body />
--- components/MHeader.js
+++ components/MHeader.js
@@ -1,18 +1,42 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
-import { Header } from "react-native-elements";
+import React, { Component, useState } from "react";
+import { Text, View, StatusBar } from "react-native";
+import { Header, SearchBar, Icon } from "react-native-elements";
 
-export class MHeader extends Component {
-  render() {
-    return (
-      <Header
-        leftComponent={{
-          text: "Moviez",
-          style: { fontSize: 20, color: "#fff" }
-        }}
-      />
-    );
-  }
-}
+export default MHeader = () => {
+  const [search, setSearch] = useState(false);
 
-export default MHeader;
+  const onSearchClick = () => {
+    setSearch(true);
+  };
+
+  const onCancelClick = () => {
+    setSearch(false);
+  };
+
+  return (
+    <View>
+      {search ? (
+        <View style={{ marginTop: StatusBar.currentHeight }}>
+          <SearchBar
+            placeholder="Type here..."
+            onCancel={onCancelClick}
+            onClear={onCancelClick}
+          />
+        </View>
+      ) : (
+        <Header
+          leftComponent={{
+            text: "Moviez",
+            style: { fontSize: 20, color: "#fff" }
+          }}
+          rightComponent={{
+            icon: "search",
+            type: "font-awesome",
+            color: "#fff",
+            onPress: onSearchClick
+          }}
+        />
+      )}
+    </View>
+  );
+};
